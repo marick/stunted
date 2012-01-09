c0cd36b1d2f7ee1783c8f45997ad5e1a8d3dfd7d
Brian Marick <marick@exampler.com>
2012-01-09 13:24:02 -0600

    Added examples

---
+++ examples/chaining-of-function-with-IMPLICIT-self.rb
@@ -0,0 +1,49 @@
+require '../lib/stunted'
+
+# A boring class that holds a value and can produce
+# new objects with an incremented value
+class ValueHolder
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def bumped
+    self.class.new(value + 1)
+  end
+end
+
+puts ValueHolder.new(0).bumped.bumped.value   # 2
+
+# Let's let this object be sent functions as if they were messages.
+class ValueHolder
+  include Stunted::Chainable
+end
+
+puts ValueHolder.new(0).
+     bumped.
+     defsend(-> { self.class.new(@value + 1000) }).
+     defsend(-> { self.class.new(value +   330) }).
+     value     # 1331
+     
+
+# The need for parentheses above is annoying, but can be avoided with blocks.
+puts ValueHolder.new(0).
+     bumped.
+     defsend { self.class.new(@value + 1000) }.
+     defsend { self.class.new(value +   330) }.
+     value     # 1331
+     
+
+# Here's an example of using higher-order functions
+
+make_adder =
+  lambda do | addend |
+    -> { self.class.new(@value + addend) }
+  end
+
+puts ValueHolder.new(0).
+     bumped.
+     defsend(make_adder.(33)).
+     value     # 34
---
+++ examples/chaining-of-function-with-explicit-passing-of-self.rb
@@ -0,0 +1,46 @@
+require '../lib/stunted'
+
+# A boring class that holds a value and can produce
+# new objects with an incremented value
+class ValueHolder
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def bumped
+    self.class.new(value + 1)
+  end
+end
+
+puts ValueHolder.new(0).bumped.bumped.value   # 2
+
+# Let's let this object be sent functions as if they were messages.
+class ValueHolder
+  include Stunted::Chainable
+end
+
+puts ValueHolder.new(0).
+     bumped.
+     pass_to(-> _ { _.value + 1000 }).     # I'm using _ to represent "self"
+     succ     # 1002
+     
+# The need for parentheses above is annoying, but can be avoided with blocks.
+puts ValueHolder.new(0).
+     bumped.
+     pass_to { | _ | _.value + 500 }.
+     succ     # 502
+
+
+# Here's an example of using higher-order functions
+
+make_adder =
+  lambda do | addend |
+    -> _ { _.value + addend }
+  end
+
+puts ValueHolder.new(0).
+     bumped.
+     pass_to(make_adder.(33)).
+     succ     # 35
