66d58e97166a5cde929eff7d9566343c7e1cdbdd
Brian Marick <marick@exampler.com>
2012-02-05 18:55:31 -0600

    Remove mocking function from FunctionalHash

--- lib/stunted/mocking.rb
+++ lib/stunted/mocking.rb
@@ -14,20 +14,8 @@ end
 module Stunted
 
   class FunctionalHash
-    def self.with_replacement_methods(hash)
-      old_pairs = hash.collect do | key, value |
-        old_method = instance_method(key)
-        define_method(key, value)      
-        [key, old_method]
-      end
-      yield
-    ensure
-      old_pairs.each { | pair | define_method(*pair) }
-    end
-
     def with_replacement_methods(hash, &block)
       self.class.with_replacement_methods(hash, &block)
     end
-
   end
 end
